b77db53f7c02fea801348a9911076cfe6719e7a1
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2023-02-05 10:34:51 +0530

    submit form

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -7,19 +7,3 @@ icon.onclick = function () {
         icon.src = "image/moon.png";
     }
 }
-
-emailjs.init('1MXNf7pjvlS8_K1nO');
-window.onload = () => {
-    document.getElementById('contact-form').setAttribute('submit', function (e) {
-        e.preventDefault();
-
-        this.contact_number.value = Math.random() * 100000 | 0;
-
-        emailjs.sendForm("service_gps9dtj", "template_vi3y2ql", this)
-            .then(function () {
-                console.log('SUCCESS!');
-            }, function (error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
